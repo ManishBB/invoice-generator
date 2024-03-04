50342199b01446deb9d4224730519bfcb91db6c3
Manish <mbb7224@gmail.com>
2024-03-04 16:56:29 +0530

    Updated base url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const conf = {
     serverUrl: String(import.meta.env.SERVER_URL),
-    baseUrl: "http://localhost:8000/api/v1",
+    baseUrl: "https://lovely-ring-deer.cyclic.app",
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
 };
 
